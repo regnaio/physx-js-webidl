90d3a63d84f88ca172885ec1b984052c04dd7720
local <local@local.local>
2025-05-17 11:58:57 -0700

    new

--- make.sh
+++ make.sh
@@ -2,7 +2,8 @@
 
 cd PhysX/physx/compiler/emscripten-release/
 rm sdk_source_bin/physx-js-webidl.*
-make -j8
+# make -j8 # Fails if machine is too fast
+make -j4
 
 cp sdk_source_bin/physx-js-webidl.js ../../../../dist/
 cp sdk_source_bin/physx-js-webidl.wasm ../../../../dist/
